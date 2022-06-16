1968c5bf2084007c59fb81c12c74d02cd2583263
Kuellar <icuellartapia@gmail.com>
2022-06-16 11:29:49 -0400

    WIP: Send data to esp gatt

--- qt_utils/configs.py
+++ qt_utils/configs.py
@@ -1,3 +1,4 @@
+import pygatt
 from db.model import Config
 from db.api_db import add_config
 STATUS_DICT = {
@@ -9,29 +10,49 @@ STATUS_DICT = {
         "BLE continua": 30,
         "BLE discontinua": 31
 }
-    
-def saveConfiguration(self):
-    self.consoleLog("Saving configuration...")
-    configs = None
-    try:
-        configs = Config(
-            Status = STATUS_DICT[self.operationModeBox.currentText()],
-            ID_Protocol = int(self.protocolIDBox.currentText()),
-            BMI270_sampling = int(self.AccSamplingBox.toPlainText()),
-            BMI270_Acc_Sensibility = int(self.AccSensibilityBox.toPlainText()),
-            BMI270_Gyro_Sensibility = int(self.GyroSensibilityBox.toPlainText()),
-            BME688_Sampling = int(self.BME688SamplingBox.toPlainText()),
-            Discontinuous_Time = int(self.discontinuosTimeBox.toPlainText()),
-            Port_TCP = int(self.portTCPBox.toPlainText()),
-            Port_UDP = int(self.portUDPBox.toPlainText()),
-            Host_Ip_Addr = int(self.hostIPAddrBox.toPlainText()),
-            Ssid = int(self.ssidBox.toPlainText()),
-            Pass = int(self.passBox.toPlainText()),
-        )
-    except:
-        self.consoleLog("Wrong configuration inputs")
-    if add_config(configs):
-        self.consoleLog("Configuration saved")
-    else:
-        self.consoleLog("Error saving configuration")
 
+def saveConfiguration(self):
+    # Send data to ESP
+    device = self.selectBTComboBox.currentText()
+    device_address = device[-17:]
+
+    adapter = pygatt.backends.GATTToolBackend()
+
+    self.consoleLog(f" Connecting to {device} ...")
+    try:
+        # CONNECT
+        adapter.start()
+        self.device = adapter.connect(device_address)
+        self.consoleLog(f" Connected to {device} ...")
+        # SEND
+        res = self.device.char_read("65281")
+        self.consoleLog(f" Sending...")
+
+        # Save data to DB
+        self.consoleLog("Saving configuration...")
+        configs = None
+        try:
+            configs = Config(
+                Status = STATUS_DICT[self.operationModeBox.currentText()],
+                ID_Protocol = int(self.protocolIDBox.currentText()),
+                BMI270_sampling = int(self.AccSamplingBox.toPlainText()),
+                BMI270_Acc_Sensibility = int(self.AccSensibilityBox.toPlainText()),
+                BMI270_Gyro_Sensibility = int(self.GyroSensibilityBox.toPlainText()),
+                BME688_Sampling = int(self.BME688SamplingBox.toPlainText()),
+                Discontinuous_Time = int(self.discontinuosTimeBox.toPlainText()),
+                Port_TCP = int(self.portTCPBox.toPlainText()),
+                Port_UDP = int(self.portUDPBox.toPlainText()),
+                Host_Ip_Addr = int(self.hostIPAddrBox.toPlainText()),
+                Ssid = int(self.ssidBox.toPlainText()),
+                Pass = int(self.passBox.toPlainText()),
+            )
+        except:
+            self.consoleLog("Wrong configuration inputs")
+        if add_config(configs):
+            self.consoleLog("Configuration saved")
+        else:
+            self.consoleLog("Error saving configuration")
+    finally:
+        adapter.stop()
+        self.device = None
+        self.consoleLog(f" Connection closed")
